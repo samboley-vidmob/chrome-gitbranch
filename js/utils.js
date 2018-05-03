a71b076b43d9d29ec9fb334d14382e56118d1e6e
Hernan Silva <hachesilva@gmail.com>
2018-05-02 22:44:54 -0500

    Convert utils lib to es6 class

--- js/utils.js
+++ js/utils.js
@@ -1,49 +1,52 @@
 (function () {
 	'use strict';
 
-	var ExtUtils = function() {
-		var self = this;
-
-		this.toastMessage = function(message, type) {
-			return self.toastMsg(message, type);
-		};
-
-		self.i18nTranslate();
-	}
-
-	ExtUtils.prototype.toastMsg = function(message, type) {
-		var $messages = document.createElement('div');
-		$messages.id = 'messages';
-
-		$messages.innerText = message;
-		$messages.className = 'visible';
-
-		if (type == 'error') {
-			$messages.className += ' error';
+	class ExtUtils {
+		constructor() {
+			var self = this;
+	
+			this.toastMessage = function(message, type) {
+				return self.toastMsg(message, type);
+			};
+	
+			self.i18nTranslate();
 		}
 
-		document.body.appendChild($messages);
-
-		setTimeout(function() {
-			$messages.className = '';
-
+		toastMsg(message, type) {
+			var $messages = document.createElement('div');
+			$messages.id = 'messages';
+	
+			$messages.innerText = message;
+			$messages.className = 'visible';
+	
+			if (type == 'error') {
+				$messages.className += ' error';
+			}
+	
+			document.body.appendChild($messages);
+	
 			setTimeout(function() {
-				document.body.removeChild($messages);
+				$messages.className = '';
+	
+				setTimeout(function() {
+					document.body.removeChild($messages);
+				}, 2000);
 			}, 2000);
-		}, 2000);
-
-	};
-
-	ExtUtils.prototype.i18nTranslate = function() {
-		var $objects = document.getElementsByTagName('*');
-
-		for(var i = 0; i < $objects.length; i++) {
-			if ($objects[i].dataset && $objects[i].dataset.translate) {
-				$objects[i].innerHTML = chrome.i18n.getMessage($objects[i].dataset.translate);
+	
+		};
+	
+		i18nTranslate() {
+			var $objects = document.getElementsByTagName('*');
+	
+			for(var i = 0; i < $objects.length; i++) {
+				if ($objects[i].dataset && $objects[i].dataset.translate) {
+					$objects[i].innerHTML = chrome.i18n.getMessage($objects[i].dataset.translate);
+				}
 			}
 		}
+	
 	}
-
+	
 	if (window.ExtUtils === undefined) {
 		window.ExtUtils = new ExtUtils();
 	}
